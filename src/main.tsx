dd25d69292bbaa989e5302072663f354022f78ca
David Wilson <szabol.david04@gmail.com>
2024-10-14 23:34:27 +0100

    Structuring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import Dashboard from './Pages/Dashboard/Dashboard'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Dashboard />
   </StrictMode>,
 )
